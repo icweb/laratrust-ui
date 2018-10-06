bd58593185c08e137465965572caa005dfcefbdb
Ian <ianconway@protonmail.com>
2018-10-06 18:43:46 -0400

    Updated routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-Route::model('trusty/users', App\User::class);
-Route::resource('trusty/users', 'Icweb\Trusty\App\Http\Controllers\UsersController');
+Route::model('users', App\User::class);
+Route::resource('users', 'Icweb\Trusty\App\Http\Controllers\UsersController');
 
-Route::model('trusty/roles', App\Role::class);
-Route::resource('trusty/roles', 'Icweb\Trusty\App\Http\Controllers\RolesController');
+Route::model('roles', App\Role::class);
+Route::resource('roles', 'Icweb\Trusty\App\Http\Controllers\RolesController');
 
-Route::model('trusty/permissions', App\Permission::class);
-Route::resource('trusty/permissions', 'Icweb\Trusty\App\Http\Controllers\PermissionsController');
+Route::model('permissions', App\Permission::class);
+Route::resource('permissions', 'Icweb\Trusty\App\Http\Controllers\PermissionsController');
 
