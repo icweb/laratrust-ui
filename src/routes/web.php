e005fd45e6aa370d47dcc63a720fd06851700daa
Ian Conway <ianconway@protonmail.com>
2019-07-07 11:47:41 -0400

    Added missing name set users destroy route

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,6 +14,7 @@ Route::group(['middleware' => ['web', 'auth']], function () {
                 'read' => 'trusty.users.read',
                 'update' => 'trusty.users.update',
                 'create' => 'trusty.users.create',
+                'destroy' => 'trusty.users.destroy',
             ]
         ]);
 
