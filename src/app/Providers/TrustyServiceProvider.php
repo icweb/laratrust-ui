781ea5425975455aaac01072016ae2f46fd8553e
Ian Conway <ianconway@protonmail.com>
2019-07-07 13:14:14 -0400

    Updated publish paths

--- src/app/Providers/TrustyServiceProvider.php
+++ src/app/Providers/TrustyServiceProvider.php
@@ -13,13 +13,13 @@ class TrustyServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '../../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
 
-        $this->loadViewsFrom(__DIR__ . '../../resources/views/', 'trusty');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views/', 'trusty');
 
         $this->publishes([
-            __DIR__ . '../../resources/views/' => resource_path('views/vendor/trusty'),
-            __DIR__ . '../../config/trusty.php' => config_path('trusty.php'),
+            __DIR__ . '/../../resources/views/' => resource_path('views/vendor/trusty'),
+            __DIR__ . '/../../config/trusty.php' => config_path('trusty.php'),
         ], 'trusty');
 
     }
